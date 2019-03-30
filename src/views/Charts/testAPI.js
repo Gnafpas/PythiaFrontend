52adbce6381353081611c343c81653fc6d635704
George <george@skamnos.com>
2019-03-30 22:50:00 +0200

    Initial

---
+++ src/views/Charts/testAPI.js
@@ -0,0 +1,48 @@
+/**
+ * Created by George on 24/03/2019.
+ */
+import React from "react";
+
+function Bla(props){
+  return <h1>{props.message}</h1>;
+}
+
+
+// mobiscroll.settings = {
+//   theme: 'ios'
+// };
+
+// class CalendarDemo extends React.Component {
+//   render () {
+//     return (
+//       <mobiscroll.Form inputStyle="box">
+//         <div className="mbsc-grid mbsc-form-grid">
+//           <div className="mbsc-row">
+//             <div className="mbsc-col-sm-12 mbsc-col-md-6">
+//               <label>
+//                 Mobile
+//                 <mobiscroll.Calendar
+//                   display="bubble"
+//                   placeholder="Please Select..."
+//                 />
+//               </label>
+//             </div>
+//             <div className="mbsc-col-sm-12 mbsc-col-md-6">
+//               <label>
+//                 Desktop
+//                 <mobiscroll.Calendar
+//                   display="bubble"
+//                   touchUi={false}
+//                   placeholder="Please Select..."
+//                 />
+//               </label>
+//             </div>
+//           </div>
+//         </div>
+//       </mobiscroll.Form>
+//     );
+//   }
+// }
+
+
+export default  Bla;
